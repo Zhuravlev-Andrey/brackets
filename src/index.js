0dfb544b8b820cdcf5a16861a779516db04a587b
Zhuravlev <zhuravlev112358@mail.ru>
2022-08-02 06:58:31 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [],
+    open = ["[", "{", "("],
+    close = ["]", "}", ")"];
+  for (let i = 0; i < str.length; i++) {
+    if (open.includes(str[i])) {
+      stack.push(str[i]);
+    } else {
+      if (close.indexOf(str[i]) === open.indexOf(stack[stack.length - 1])) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
